387b4ac3ff7628345666d7e5b7d0eae1df2cede5
Девятка <9ka.2011@gmail.com>
2025-04-17 21:13:34 +0500

    завешил первый функционал программы

--- imitator.py
+++ imitator.py
@@ -1,17 +1,20 @@
 from flask import Flask, abort
 
+
 app = Flask(__name__)
+
 
 @app.route('/protect/rb<int:relay><action>.cgi')
 def handle_request(relay, action):
     if relay < 0 or relay > 15:
-        abort(404)
+        abort(400)
 
     if action not in ['n', 'f', 's']:
-        abort(404)
+        abort(400)
 
     print(f'Реле {relay}: действие {action}')
     return f'ОК: Реле {relay}, действие {action}'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import requests
 
 
 def command(ip, action, relay):
-    response = requests.get(f'http://{ip}/protect/rb{relay}{action}.cgi')
-    print(f'отправлен запрос по адресу: http://{ip}/protect/rb{relay}{action}.cgi')
+    address = f'http://{login}:{password}@{ip}/protect/rb{relay}{action}.cgi'
+    print('Отправляется запрос по адресу:', address)
+    response = requests.get(address)
+    print('Успешно отправлен запрос по адресу:', address)
 
-ip = 'localhost'
-# ip = input('Введите IP: ')
-# login = input('Логин: ')
-# password = input('Пароль: ')
+
+ip = input('Введите IP: ')
+login = input('Логин: ')
+password = input('Пароль: ')
 while True:
     relay = input('Выберите реле: ')
     action = input('Выберите действие (n/f/s): ')
